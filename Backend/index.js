c98c4ca2ffaf0bad4ca9e1676a1f0530aa7ed969
Rohan shah <rohankumar.shah@sjsu.edu>
2019-03-08 23:32:49 -0800

    object is not a function error !

--- Backend/index.js
+++ Backend/index.js
@@ -1,90 +1,17 @@
-const express=require('express');
-const app=express();
-var methodOverride=require('express-method-override');
-const bodyParser=require('body-parser');
-const session=require('express-session');
-const cookieParser=require('cookie-parser');
-var cors = require('cors');
-var port=process.env.PORT||3001
+var express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+var app = express()
+var port = process.env.PORT || 3001
 
-const signupRoute=require("./routes/signup");
-const loginRoute=require("./routes/login");
-const logoutRoute=require("./routes/logout")
-const dashboardRoute=require("./routes/dashboard");
-const coursesRoute=require("./routes/courses")
-const Users=require("./routes/Users");
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(methodOverride('_method'));
-app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.json());
-app.use(cookieParser());
-//install cross origin reference 
-//app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use(cors());
-app.use(session({
-    key:'user_sid',
-    secret:'lab1 pro',
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        expires:30*1000*60
-    }
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-}));
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-  });
+var Users = require('./routes/Users')
 
+app.use('/users', Users)
 
-app.use((req,res,next)=>{
-    if(req.cookies.user_sid && !req.session.user){
-        res.clearCookie('user_sid');
-    }
-    next();
-});
-app.get('/',function(req,res){ 
-    res.render('login');
-    
+app.listen(port, () => {
+    console.log("Server is running on port: " + port)
 })
-
-app.use('/users',Users)
-
-/*var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    database : 'Canvas',
-    user     : 'root',
-    password : '',
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('Error connecting: ' + err.stack);
-        return;
-    }
-
-    console.log('Connected as id ' + connection.threadId);
-}); 
-
-connection.query('SELECT * FROM users', function (error, results, fields) {
-    if (error)
-        throw error;
-
-    results.forEach(result => {
-        console.log(result);
-    });
-}); */
-
-//connection.end();
-//This example creates a MySQL connection object that connects to the MySQL database. A
-/*app.use("/signup",signupRoute)
-app.use("/login",loginRoute)
-app.use("logout",logoutRoute)
-app.use("/dashboard",dashboardRoute)
-app.use("/courses",coursesRoute)*/
-app.listen(port, _=>console.log("server running on 3001"));
--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -4,13 +4,13 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
-router.get('/', function (req, res) {
+/*router.get('/', function (req, res) {
     if (req.session.user) {
         res.render('');
     } else {
         res.render('login');
     }
-});
+}); */
 
 /*router.post('/', function (req, res) {
     if (req.session.user) {
